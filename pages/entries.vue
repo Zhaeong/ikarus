99eba72da6e1b646001ef57bb1594b43f75b2b61
= <zhaeong@github.com>
2021-01-28 22:08:47 -0500

    routing

--- pages/entries.vue
+++ pages/entries.vue
@@ -1,22 +1,33 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.id">
+  <v-card class="mx-auto" max-width="700" tile>
+    <v-list rounded outlined>
+      <v-list-item-group color="primary">
+        <v-list-item
+          :to="{ name: 'entry', params: { id: item.id } }"
+          two-line
+          v-for="item in items"
+          :key="item.id"
+        >
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+
+            <v-divider></v-divider>
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-    </v-col>
-  </v-row>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 <script>
 export default {
   components: {},
   async asyncData(context) {
     var items = new Array();
-    var row = { id: "a", title: "heyyya" };
+    var row = {
+      id: "a",
+      title:
+        "heyyya sdfskdjlfjsdlkjfds fsd fsd fsdlk fsd fsd fjsldkf sdl fjsld jfsd f"
+    };
     items.push(row);
     row = { id: "2", title: "dkfdk" };
     items.push(row);
---
+++ pages/entry.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="mx-auto" max-width="700" tile>
+    {{ out }}
+  </v-card>
+</template>
+<script>
+export default {
+  components: {},
+  async asyncData(context) {
+    console.log(context.params);
+    var out = context.params.id;
+    return { out };
+  }
+};
+</script>
