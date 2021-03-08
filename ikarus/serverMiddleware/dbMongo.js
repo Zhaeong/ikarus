3ba98ccbccd22af5896891c9086055fa371574de
= <zhaeong@github.com>
2021-03-08 01:05:23 -0500

    dateee

--- ikarus/serverMiddleware/dbMongo.js
+++ ikarus/serverMiddleware/dbMongo.js
@@ -40,7 +40,47 @@ export default {
   insertFire: async function(sTitle, sContent) {
     let mongoObj = new MongoObj(uri);
     try {
-      var insertData = { title: sTitle, content: sContent, type: "Fire" };
+      let date_ob = new Date();
+
+      // adjust 0 before single digit date
+      let date = ("0" + date_ob.getDate()).slice(-2);
+
+      // current month
+      let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+      // current year
+      let year = date_ob.getFullYear();
+
+      // current hours
+      let hours = date_ob.getHours();
+
+      // current minutes
+      let minutes = date_ob.getMinutes();
+
+      // current seconds
+      let seconds = date_ob.getSeconds();
+
+      let dateTime =
+        year +
+        "-" +
+        month +
+        "-" +
+        date +
+        " " +
+        hours +
+        ":" +
+        minutes +
+        ":" +
+        seconds;
+
+      // prints date & time in YYYY-MM-DD HH:MM:SS format
+      console.log("datebe: ", dateTime);
+      var insertData = {
+        title: sTitle,
+        content: sContent,
+        type: "Fire",
+        date: dateTime
+      };
       await mongoObj.connect();
 
       var response = await mongoObj.collection
